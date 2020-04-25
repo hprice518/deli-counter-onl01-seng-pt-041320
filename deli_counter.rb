038e57fc9c81f468c1de9ce9d10eb35a7ecea373
Harrison Price <hprice518@gmail.com>
2020-04-25 16:27:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli= []
+
+def line(katz_deli) 
+  if katz_deli.length == 0 
+  puts "The line is currently empty."
+  else 
+   new_line = []
+    katz_deli.each_with_index { |name, x| new_line << "#{x+1}. #{name} "}
+  puts "The line is currently: " << new_line.join() 
+  end
+  
+end
+
+
+
+
+
+   
