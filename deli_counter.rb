f9722fb071471ca424311a26a81556c8083909ae
Harrison Price <hprice518@gmail.com>
2020-04-27 14:52:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,13 +11,20 @@
 #         end
 #         puts line_order
 #     end
+<<<<<<< HEAD
 # end
+=======
+
+
+
+>>>>>>> 2a08c42566fe93fc087c53f6f1fbb28e31521bcd
 
 def line(katz_deli) 
   
   if katz_deli.length == 0 
   puts "The line is currently empty."
   else 
+<<<<<<< HEAD
     new_line = "The line is currently:"
     katz_deli.each_with_index do |name, x| 
       new_line = new_line + " #{x + 1}. " + name
@@ -32,6 +39,21 @@ def take_a_number(array, string)
     puts "Welcome, #{string}. You are number #{array.length} in line."
 end
 
+=======
+   new_line = "The line is currently:"
+   katz_deli.each_with_index do |name, x| 
+      puts new_line = new_line + " #{x + 1}. #{name}."
+    end
+
+  end
+end
+
+def take_a_number(array, string)
+  array << string 
+    puts "Welcome, #{string}. You are number #{array.length} in line."
+end
+
+>>>>>>> 2a08c42566fe93fc087c53f6f1fbb28e31521bcd
 def now_serving(array)
   if array.length == 0 
     puts "There is nobody waiting to be served!"
